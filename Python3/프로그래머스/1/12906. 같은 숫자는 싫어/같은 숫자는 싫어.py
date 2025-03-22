a6c9b9d687517306bb0cd1aeb72060359d13f7a8
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-03-22 15:32:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 78.99 ms, Memory: 27.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = [arr[0]]
+    prev = arr[0]
+    for i in range(1, len(arr)):
+        if arr[i] == prev:
+            continue
+        answer.append(arr[i])
+        prev = arr[i]
+    return answer
