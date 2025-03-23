ab592e1b6c1bbd0264adb24f04b7c3518d9ff7a4
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-03-24 08:53:26 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ JavaScript/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,13 +1,6 @@
 function solution(lottos, win_nums) {
     var answer = [];
-    const rank = new Map();
-    rank.set(6, 1);
-    rank.set(5, 2);
-    rank.set(4, 3);
-    rank.set(3, 4);
-    rank.set(2, 5);
-    rank.set(1, 6);
-    rank.set(0, 6);
+    var rank = [6,6,5,4,3,2,1]
     let cnt_w = 0;
     let cnt_z = 0;
     for (let i = 0; i<lottos.length; i++) {
@@ -16,8 +9,8 @@ function solution(lottos, win_nums) {
         }
         if (lottos[i] == 0) cnt_z += 1;
     }
-    answer.push(rank.get(cnt_w + cnt_z));
-    answer.push(rank.get(cnt_w));
+    answer.push(rank[cnt_w+cnt_z]);
+    answer.push(rank[cnt_w]);
     
     return answer;
 }
