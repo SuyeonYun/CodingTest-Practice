a3efe87f8a8fcbf69dadbc764950488e167b194b
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-03-27 10:04:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.01 ms, Memory: 32.9 MB -BaekjoonHub

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,13 +1,10 @@
 def solution(participant, completion):
     part_dict = {}
-    for p in participant:
-        if p in part_dict:
-            part_dict[p] += 1
-        else:
-            part_dict[p] = 1
-            
-    for c in completion:
-        part_dict[c] -= 1
+    for i in range(len(participant)):
+        part_dict[participant[i]] = (part_dict.get(participant[i]) or 0) + 1
+        
+        if i < len(completion):
+            part_dict[completion[i]] = (part_dict.get(completion[i]) or 0) - 1
     
     for ret in part_dict.keys():
         if part_dict[ret] > 0:
