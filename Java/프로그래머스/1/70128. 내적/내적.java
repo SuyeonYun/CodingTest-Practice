f8811053570d13309af5bf9a7385634fe3ae1b27
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-03-30 10:04:56 +0900

    [level 1] Title: 내적, Time: 0.06 ms, Memory: 91.4 MB -BaekjoonHub

--- Java/프로그래머스/1/70128. 내적/내적.java
+++ Java/프로그래머스/1/70128. 내적/내적.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int[] a, int[] b) {
-        int ans = 0;
+        int answer = 0;
         
         for (int i=0; i< a.length; i++) {
-            ans += a[i] * b[i];
+            answer += a[i] * b[i];
         }
-        return ans;
+        return answer;
     }
 }
