ab75107f05dbc05a3c264158a64c6ce7a9845916
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-04-02 08:07:32 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,15 @@
+function solution(array, commands) {
+    var answer = [];
+    var temp = [];
+    let i,j,k;
+    for (com of commands) {
+        i = com[0] - 1;
+        j = com[1];
+        k = com[2] - 1;
+        
+        temp = array.slice(i,j);
+        temp.sort((a,b) => a-b);
+        answer.push(temp[k])
+    }
+    return answer;
+}
