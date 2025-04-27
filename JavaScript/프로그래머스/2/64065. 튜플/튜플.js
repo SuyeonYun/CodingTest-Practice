98f3c24e57ababdc07f9ead90654dcc32dc4bf41
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-04-27 15:02:33 +0900

    [level 2] Title: 튜플, Time: 58.26 ms, Memory: 53.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/64065. 튜플/튜플.js
+++ JavaScript/프로그래머스/2/64065. 튜플/튜플.js
@@ -6,7 +6,9 @@ function solution(s) {
         map.set(i, (map.get(i) || 0) + 1);
     };
     
-    const sorted = [...map.entries()].sort((a, b) => b[1] - a[1]);    
-    var answer = sorted.map(([key, value]) => Number(key));
+    const len = map.size;
+    var answer = new Array(len);
+    
+    map.forEach((value, key) => answer[len - value] = parseInt(key));
     return answer;
 }
