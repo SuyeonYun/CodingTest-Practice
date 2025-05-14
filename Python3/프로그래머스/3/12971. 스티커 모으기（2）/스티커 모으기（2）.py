979474c77dca1671c09b6e19f83d48efa3ff1eb5
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-05-14 13:06:22 +0900

    [level 3] Title: 스티커 모으기(2), Time: 61.17 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -0,0 +1,20 @@
+def solution(sticker):
+    answer = 0
+    n = len(sticker)
+
+    if n <= 3:
+        return max(sticker)
+    
+    dp1 = []
+    dp1.append(sticker[0])
+    dp1.append(sticker[0])
+    for i in range(2, n - 1):
+        dp1.append(max(dp1[i - 2] + sticker[i], dp1[i - 1]))
+    
+    dp2 = []
+    dp2.append(0)
+    dp2.append(sticker[1])
+    for i in range(2, n):
+        dp2.append(max(dp2[i - 2] + sticker[i], dp2[i - 1]))
+
+    return max(dp1[-1], dp2[-1])
