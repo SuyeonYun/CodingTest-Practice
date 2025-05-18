fa7bfd86883b93e3aca720e477e050909aeacdda
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-05-18 19:52:20 +0900

    [level 3] Title: 가장 먼 노드, Time: 636.59 ms, Memory: 23.7 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,28 @@
+def solution(n, edge):
+    q = []
+    edges = [[] * x for x in range(n)]
+    visited = [False * x for x in range(n)]
+    dist = [float('inf') * x for x in range(n)]
+    
+    q.append(0)
+    dist[0] = 0
+    
+    for s, e in edge: 
+        edges[s - 1].append(e - 1)
+        edges[e - 1].append(s - 1)
+    
+    for arr in edges:
+        arr.sort()
+
+    while len(q) > 0:
+        #print(f'q: {q}, dist: {dist}')
+        cur = q.pop(0)
+        if not visited[cur]:
+            visited[cur] = True
+            for i in edges[cur]:
+                dist[i] = min(dist[i], dist[cur] + 1)
+                q.append(i)
+        #print(f'q: {q}, dist: {dist}\n')
+    
+    max_d = max(dist)
+    return dist.count(max_d)
