d2a79e820219c963e00c857cfde5462699e7d98a
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-05-24 15:08:43 +0900

    [level 3] Title: 네트워크, Time: 0.77 ms, Memory: 9.26 MB -BaekjoonHub

--- Python3/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python3/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,24 +1,26 @@
+from collections import deque
 def solution(n, computers):
-    visited = [False] * n
-    root_arr = []
-    result = 0
-
-    def dfs(idx, root):
-        nonlocal result
-        visited[idx] = True
-        count = 0
-        for i in range(n):
-            if computers[idx][i] == 1 and not visited[i]:
-                count += 1
-                dfs(i, root)
-        if count == 0 and root not in root_arr:
-            result += 1
-            root_arr.append(root)
-        return
-    
+    answer = 0
+    q = deque()
+    visited = [False for _ in range(n)]
     for i in range(n):
-        if not visited[i]:
-            dfs(i, i)        
-    return result
+        computers[i][i] = 0
     
+    while False in visited:
+        answer += 1
+        q.append(visited.index(False))
         
+        while q:
+            cur = q.popleft()
+            if visited[cur]:
+                continue
+            visited[cur] = True
+            
+            for i in range(n):
+                if not visited[i] and computers[cur][i]:
+                    q.append(i)
+                      
+    return answer
+                    
+            
+            
