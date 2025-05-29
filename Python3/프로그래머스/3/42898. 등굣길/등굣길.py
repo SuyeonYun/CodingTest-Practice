e48b181d0b5223c70668681212fc635f114b38c0
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-05-29 11:24:46 +0900

    [level 3] Title: 등굣길, Time: 6.62 ms, Memory: 9.3 MB -BaekjoonHub

--- Python3/프로그래머스/3/42898. 등굣길/등굣길.py
+++ Python3/프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,16 +1,13 @@
 def solution(m, n, puddles):
-    ways = [[0 for _ in range(m)] for _ in range(n)]
-    ways[0][0] = 1
-        
-    for h in range(n):
-        for w in range(m):
-            if [w + 1, h + 1] in puddles:
-                ways[h][w] = -1
-                continue
-            if 0 <= h - 1 and ways[h - 1][w] >= 0:
-                ways[h][w] += ways[h - 1][w]
-            if 0 <= w - 1 and ways[h][w - 1] >= 0:
-                ways[h][w] += ways[h][w - 1]
+    info = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+    info[1][1] = 1
     
-    result = (ways[n - 1][m - 1]) % 1_000_000_007
-    return result
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if [i , j] == [1, 1]:
+                continue
+            info[i][j] = info[i - 1][j] + info[i][j - 1]
+            if [j, i] in puddles:
+                info[i][j] = 0
+                
+    return info[n][m] % 1_000_000_007
