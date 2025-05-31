5eae56eb7d7c5ced6320c86d646234e85863b2a3
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-05-31 16:03:22 +0900

    [level 3] Title: 최고의 집합, Time: 2.23 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ Python3/프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -1,17 +1,13 @@
-import math
-
 def solution(n, s):
     answer = []
-    result = 0
     
     if n > s:
         return [-1]
     
     while n > 0:
-        result = math.floor(s / n)
-        answer.append(result)
-        s -= result
+        temp = int(s / n)
+        answer.append(temp)
+        s -= temp
         n -= 1
     
-    answer.sort()
     return answer
