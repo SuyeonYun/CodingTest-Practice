0800c5bc3b239c4bb735bca0399b3a4ffbc2ed3a
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-06-02 13:32:01 +0900

    [level 3] Title: 기지국 설치, Time: 2.12 ms, Memory: 9.62 MB -BaekjoonHub

--- Python3/프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ Python3/프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -2,16 +2,14 @@ import math
 
 def solution(n, stations, w):
     answer = 0
-    rest = []
-    
-    start = 0
-    for s in stations:
-        rest.append(s - w - 1 - start)
-        start = s + w
-    if start < n:
-        rest.append(n - start)
+    start = 0 - w
+    if stations[-1] != n:
+        stations.append(n + w + 1)
         
-    for r in rest:
-        answer += math.ceil(r / (w * 2 + 1))
+    for end in stations:
+        cnt = end - start - 1 - 2 * w
+        if cnt > 0:
+            answer += math.ceil((cnt) / (2 * w + 1))
+        start = end
 
     return answer
