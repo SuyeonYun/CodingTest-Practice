83f52d8acb51e82a64768af0a688bb23a5f00bf6
SuyeonYun <102856399+SuyeonYun@users.noreply.github.com>
2025-06-11 11:38:25 +0900

    [level 3] Title: 여행경로, Time: 627.97 ms, Memory: 9.47 MB -BaekjoonHub

--- Python3/프로그래머스/3/43164. 여행경로/README.md
+++ Python3/프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 9.29 MB, 시간: 2400.04 ms
+메모리: 9.47 MB, 시간: 627.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 21일 17:18:57
+2025년 06월 11일 11:38:22
 
 ### 문제 설명
 
--- Python3/프로그래머스/3/43164. 여행경로/여행경로.py
+++ Python3/프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,49 +1,62 @@
 import copy
+
+def isAllZero(double_list, n):
+    for i in range(n):
+        for j in range(n):
+            if double_list[i][j] != 0:
+                return False
+    return True
+
 def solution(tickets):
+    idx = 0
+    a_dict = {}
+    r_dict = {}
     answer = []
     temp = set()
-    d_dict = {}
-    r_dict = {}
     
-    for s, e in tickets:
-        temp.add(s)
-        temp.add(e)
+    for a, b in tickets:
+        temp.add(a)
+        temp.add(b)
     temp = list(temp)
     temp.sort()
-
-    for i in range(len(temp)):
-        d_dict[temp[i]] = i
     
-    for i in range(len(tickets)):
-        tickets[i][0] = d_dict[tickets[i][0]]
-        tickets[i][1] = d_dict[tickets[i][1]]
-        
-    for k, v in d_dict.items():
-        r_dict[v] = k
+    for t in temp:
+        a_dict[t] = idx
+        r_dict[idx] = t
+        idx += 1
     
-    n = len(d_dict)
-        
-    INFO = [[0 for _ in range(n)] for _ in range(n)] 
+    l = len(a_dict)
+    start = a_dict['ICN']
     
-    for s, e in tickets:
-        INFO[s][e] += 1
-
-    def dfs(start, info, result):
+    info = [[0 for _ in range(l)] for _ in range(l)]
+    for a, b in tickets:
+        info[a_dict[a]][a_dict[b]] += 1
+    
+    def dfs(cur_idx, result):
+        nonlocal info
         nonlocal answer
-        result.append(start)
-        if not any(x > 0 for row in info for x in row):
-            if not answer:
-                #print(f'start: {start}, info: {info}, result: {result}')
+        
+        if isAllZero(info, l):
+            if len(answer) == 0:
                 answer = result
-        for end in range(n):
-            if info[start][end] > 0:
-                info[start][end] -= 1
-                dfs(end, copy.deepcopy(info), copy.deepcopy(result))
-                info[start][end] += 1
-                
-    dfs(d_dict['ICN'], INFO, [])
+            return
+        
+        cnt = 0
+        for i in range(l):
+            if info[cur_idx][i] > 0:
+                cnt += 1
+                info[cur_idx][i] -= 1
+                temp = copy.deepcopy(result)
+                temp.append(i)
+                dfs(i, temp)
+                info[cur_idx][i] += 1
+        if cnt == 0:
+            return
+    
+    dfs(start, [start])
     
     for i in range(len(answer)):
         answer[i] = r_dict[answer[i]]
 
     return answer
+        
